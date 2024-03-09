3db004a0872bfb8882bd08c66a0d26a9cebcd378
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-03-09 21:33:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+
+  // Static method to capitalize the first letter of a string
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  // Static method to sanitize a string
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  // Static method to titleize a string
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return str.split(' ').map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    }).join(' ');
+  }
 }
